687ae4adbad242e290169a9e97fd95d53fbb55bd
Aziza Altyyeva <altyyevaa@berea.edu>
2024-10-06 14:14:57 -0400

    coded p3_s1

--- week_3/s1/p3.py
+++ week_3/s1/p3.py
@@ -1,19 +1,24 @@
-def pangram(string):
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# def pangram(string):
+#     alphabet = 'abcdefghijklmnopqrstuvwxyz'
    
     
-    string = list(set(string))
-    #print(string)
-    count = 0
-    for i in range(0, len(string)):
-        for j in range(0, len(alphabet)):
-            if string[i] == alphabet[j]:
-                #print(string[i])
-                count += 1
-    if count == 26:
-        return True
-    else:
-        return False 
+#     string = list(set(string))
+#     #print(string)
+#     count = 0
+#     for i in range(0, len(string)):
+#         for j in range(0, len(alphabet)):
+#             if string[i] == alphabet[j]:
+#                 #print(string[i])
+#                 count += 1
+#     if count == 26:
+#         return True
+#     else:
+#         return False 
+
+def pangram(string):
+    alphabet = set('abcdefghijklmnopqrstuvwxyz')  # Step 1
+    return alphabet.issubset(set(string.lower()))  # Step 2
+
     
 
 if '__main__' == __name__:
