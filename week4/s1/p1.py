8ee3e1e0ca16f046df3c2f27a6b0530c19324f11
Aziza Altyyeva <altyyevaa@berea.edu>
2024-10-15 22:06:19 -0400

    week5 s1 coded

---
+++ week4/s1/p1.py
@@ -0,0 +1,13 @@
+"""
+Understand:
+ using two pointers. We will return a copy of the list taken in from
+ parameters in a reversed order.
+ are we copying the list? or modifying the current list?
+Plan:
+ set up two pointer variables start, end
+ loop through the list
+ swap the start and end value pointers
+ a = b
+ b = a
+"""
+#def reverse_list():
